e1bdaca21d4760ed78b3391c15240a36606831ff
Dragan Roganović <unrealityxi@gmail.com>
2018-11-09 00:05:45 +0100

    Add animated background using particles.js

--- js/creative.js
+++ js/creative.js
@@ -6,6 +6,120 @@
 
 (function($) {
     "use strict"; // Start of use strict
+    //loads particles.js
+
+    particlesJS('portfolio', {
+        "particles": {
+          "number": {
+            "value": 20,
+            "density": {
+              "enable": false,
+              "value_area": 800
+            }
+          },
+          "color": {
+            "value": "#666666"
+          },
+          "shape": {
+            "type": "circle",
+            "stroke": {
+              "width": 0,
+              "color": "#666666"
+            },
+            "polygon": {
+              "nb_sides": 5
+            },
+            "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+            }
+          },
+          "opacity": {
+            "value": 0.8,
+            "random": false,
+            "anim": {
+              "enable": false,
+              "speed": 0.7,
+              "opacity_min": 0.1,
+              "sync": false
+            }
+          },
+          "size": {
+            "value": 5,
+            "random": true,
+            "anim": {
+              "enable": true,
+              "speed": 10,
+              "size_min": 0.1,
+              "sync": false
+            }
+          },
+          "line_linked": {
+            "enable": true,
+            "distance": 3000,
+            "color": "#333333",
+            "opacity": 0.5,
+            "width": 1
+          },
+          "move": {
+            "enable": true,
+            "speed": 3,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "bounce",
+            "bounce": true,
+            // "attract": {
+            //   "enable": false,
+            //   "rotateX": 600,
+            //   "rotateY": 1200
+            // }
+          }
+        },
+        "interactivity": {
+          "detect_on": "canvas",
+          "events": {
+            "onhover": {
+              "enable": false,
+              "mode": "grab"
+            },
+            "onclick": {
+              "enable": false,
+              "mode": "push"
+            },
+            "resize": false
+          },
+          "modes": {
+            "grab": {
+              "distance": 300,
+              "line_linked": {
+                "opacity": 0.7485830392471388
+              }
+            },
+            "bubble": {
+              "distance": 400,
+              "size": 40,
+              "duration": 2,
+              "opacity": 8,
+              "speed": 3
+            },
+            "repulse": {
+              "distance": 200,
+              "duration": 0.4
+            },
+            "push": {
+              "particles_nb": 4
+            },
+            "remove": {
+              "particles_nb": 2
+            }
+          }
+        },
+        "retina_detect": true
+      });
+      
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
